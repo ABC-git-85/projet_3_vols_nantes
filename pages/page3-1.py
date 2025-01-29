98df5e42785b50bfe6aeb577dfef3765e3c86bbd
ABC-git-85 <amandine.besse44@gmail.com>
2025-01-29 12:01:23 +0100

    Prix des sièges

---
+++ pages/page3-1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+################################ CONF PAGE ################################
+
+st.set_page_config(
+    layout="centered"  # Mode par défaut
+)
+
+###########################################################################
+
+st.title("Page 3")
+st.write("Contenu de la troisième page.")
